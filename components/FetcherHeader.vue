9b904ca61105ee6aeb1975f0390e51948c912f2f
W0RNut <worasitipon.t@gmail.com>
2018-06-15 15:33:29 +0700

    init

---
+++ components/FetcherHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <header class="row" :class="bgColor">
+      <div class="col"><h1 class="text-title">{{title}}</h1></div>
+      <nuxt-link :to="uriPath" class="back-btn">
+        <span>Back</span>
+      </nuxt-link>
+    </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    uriPath: String,
+    bgColor: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+
+header.row {
+  height: 90px;
+  padding: 0;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.row {
+  lost-flex-container: row;
+}
+
+.col {
+  lost-flex-container: column;
+}
+
+.text {
+  &-title {
+    font-size: 2.5rem;
+    color: white;
+    padding: 1rem;
+  }
+}
+
+.bg {
+  &-blue {
+    background-color: $color-blue;
+  }
+
+  &-red {
+    background-color: lighten($color-red, 10);
+  }
+}
+
+.back-btn {
+  width: 100px;
+  height: 100%;
+  font-size: 1rem;
+  color: white;
+  background-color: tomato;
+  border: none;
+  text-decoration: none;
+  lost-align: middle-center;
+  &:hover {
+    background-color: darken(tomato, 10);
+  }
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,157 @@
+<template>
+  <section class="container-page">
+    <div class="container-box">
+      <div class="box">
+        <img src="~assets/images/show_me_the_data.jpg" alt="">
+      </div>
+      <div class="box">
+        <div class="col">
+          <div class="content">
+            <h1 class="text-topic">LV 1</h1>
+            <h4 class="text-subtitle">Fetch and render</h4>
+          </div>
+        </div>
+        <div class="col box-link bg-blue">
+          <nuxt-link to="/fetcher/basic" class="text-link hover-blue">GO</nuxt-link>
+        </div>
+      </div>
+      <div class="box">
+        <div class="col move-tail">
+          <div class="content">
+            <h1 class="text-topic">LV 2</h1>
+            <h4 class="text-subtitle">Chunks</h4>
+          </div>
+        </div>
+        <div class="col box-link bg-green move-head">
+          <nuxt-link to="/fetcher/chunks" class="text-link hover-blue">GO</nuxt-link>
+        </div>
+      </div>
+      <div class="box">
+        <div class="col">
+          <div class="content">
+            <h1 class="text-topic">LV 3</h1>
+            <h4 class="text-subtitle">Catch me (an error) if you can </h4>
+          </div>
+        </div>
+        <div class="col box-link bg-red">
+          <nuxt-link to="/fetcher/catch_me" class="text-link hover-blue">GO</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+@import '@/assets/scss/_respond_to.scss';
+@import '@/assets/scss/_flex.scss';
+
+img {
+  width: 100%;
+}
+
+button {
+  &.prototype {
+    width: 100%;
+    max-width: 200px;
+    height: auto;
+    border: 2px solid grey;
+    padding: 0.5rem 1rem;
+  }
+
+  &.link-btn {
+    @extend button.prototype;
+    height: 150px;
+    max-width: 150px;
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 1.5rem;
+    color: tomato;
+    border: 5px solid tomato;
+    background-color: transparent;
+
+    &:hover {
+      color: white;
+      background-color: tomato;
+    }
+  }
+}
+
+.box {
+  lost-flex-container: row;
+
+  .col {
+    height: 200px;
+    lost-column: 1/2 2 0 flex;
+    lost-align: middle-center;
+  }
+
+  .move {
+    &-head {
+      lost-move: -1/2 0 0;
+    }
+    &-tail {
+      lost-move: 1/2 0 0;
+    }
+  }
+
+  &-link {
+    // border: 1rem solid $color-dark-grey;
+  }
+}
+
+.content {
+  position: relative;
+}
+
+.text {
+  &-topic {
+    font-size: 3rem;
+    color: #2d3436;
+
+    @media (--xs) {
+      font-size: 2rem;
+    }
+  }
+
+  &-link {
+    font-size: 1.5rem;
+    // text-decoration: none;
+    color: $color-light-grey;
+
+    &.hover {
+      &-blue {
+        &:hover {
+          color: darken($color-blue, 10);
+        }
+      }
+      &-green {
+        &:hover {
+          color: darken($color-green, 10);
+        }
+      }
+      &-red {
+        &:hover {
+          color: darken($color-red, 10);
+        }
+      }
+    }
+  }
+}
+
+.bg {
+  &-blue {
+    background-color: $color-blue;
+    border: 0.85rem solid darken($color-blue, 7);
+  }
+  &-green {
+    background-color: $color-green;
+    border: 0.85rem solid darken($color-green, 7);
+  }
+  &-red {
+    background-color: $color-red;
+    border: 0.85rem solid darken($color-red, 7);
+  }
+}
+</style>
+
